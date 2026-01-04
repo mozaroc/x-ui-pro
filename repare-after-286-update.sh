154112a3d8aa2b461e00df100edc863c3f06acc4
Ivan Razin <mozaroc@gmail.com>
2026-01-04 22:39:47 +0200

    Create repare-after-286-update.sh

---
+++ repare-after-286-update.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+while true; do	
+	if [[ -n "$domain" ]]; then
+		break
+	fi
+	echo -en "Enter your panel domain(sub.domain.tld): " && read domain 
+done
+
+XUIPORT=$(sqlite3 -list $XUIDB 'SELECT "value" FROM settings WHERE "key"="webPort" LIMIT 1;' 2>&1)
+XUIPATH=$(sqlite3 -list $XUIDB 'SELECT "value" FROM settings WHERE "key"="webBasePath" LIMIT 1;' 2>&1)
+
+mkdir -p /root/cert/${domain}
+chmod 755 /root/cert/*
+
+ln -s /etc/letsencrypt/live/${domain}/fullchain.pem /root/cert/${domain}/fullchain.pem
+ln -s /etc/letsencrypt/live/${domain}/privkey.pem /root/cert/${domain}/privkey.pem
+
+cp /etc/nginx/sites-available/${domain} /root/${domain}.backup
+rm -rf /etc/nginx/sites-available/${domain}
+
+cat > "/etc/nginx/sites-available/${domain}" << EOF
+server {
+	server_tokens off;
+	server_name ${domain};
+	listen 7443 ssl http2 proxy_protocol;
+	listen [::]:7443 ssl http2 proxy_protocol;
+	index index.html index.htm index.php index.nginx-debian.html;
+	root /var/www/html/;
+	ssl_protocols TLSv1.2 TLSv1.3;
+	ssl_ciphers HIGH:!aNULL:!eNULL:!MD5:!DES:!RC4:!ADH:!SSLv3:!EXP:!PSK:!DSS;
+	ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+	ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+	if (\$host !~* ^(.+\.)?$domain\$ ){return 444;}
+	if (\$scheme ~* https) {set \$safe 1;}
+	if (\$ssl_server_name !~* ^(.+\.)?$domain\$ ) {set \$safe "\${safe}0"; }
+	if (\$safe = 10){return 444;}
+	if (\$request_uri ~ "(\"|'|\`|~|,|:|--|;|%|\\$|&&|\?\?|0x00|0X00|\||\\|\{|\}|\[|\]|<|>|\.\.\.|\.\.\/|\/\/\/)"){set \$hack 1;}
+	error_page 400 401 402 403 500 501 502 503 504 =404 /404;
+	proxy_intercept_errors on;
+	#X-UI Admin Panel
+	location /${XUIPATH}/ {
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+
+        proxy_set_header Host \$host;
+		proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
+
+        proxy_read_timeout 3600s;
+        proxy_send_timeout 3600s;
+
+        proxy_pass https://127.0.0.1:${XUIPORT};
+		break;
+	}
+        location /${XUIPATH} {
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+
+        proxy_set_header Host \$host;
+		proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
+
+        proxy_read_timeout 3600s;
+        proxy_send_timeout 3600s;
+
+        proxy_pass https://127.0.0.1:${XUIPORT};
+		break;
+	}
+	include /etc/nginx/snippets/includes.conf;
+
+}
+EOF
+
+cat > "/etc/nginx/snippets/includes.conf" << EOF
+  	#sub2sing-box
+	location /${sub2singbox_path}/ {
+		proxy_redirect off;
+		proxy_set_header Host \$host;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_pass http://127.0.0.1:8080/;
+		}
+    # Path to open clash.yaml and generate YAML
+    location ~ ^/${web_path}/clashmeta/(.+)$ {
+        default_type text/plain;
+        ssi on;
+        ssi_types text/plain;
+        set \$subid \$1;
+        root /var/www/subpage;
+        try_files /clash.yaml =404;
+    }
+    # web
+    location ~ ^/${web_path} {
+        root /var/www/subpage;
+        index index.html;
+        try_files \$uri \$uri/ /index.html =404;
+    }
+ 	#Subscription Path (simple/encode)
+        location /${sub_path} {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	location /${sub_path}/ {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	location /assets/ {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	location /assets {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	#Subscription Path (json/fragment)
+        location /${json_path} {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	location /${json_path}/ {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+        #XHTTP
+        location /${xhttp_path} {
+          grpc_pass grpc://unix:/dev/shm/uds2023.sock;
+          grpc_buffer_size         16k;
+          grpc_socket_keepalive    on;
+          grpc_read_timeout        1h;
+          grpc_send_timeout        1h;
+          grpc_set_header Connection         "";
+          grpc_set_header X-Forwarded-For    \$proxy_add_x_forwarded_for;
+          grpc_set_header X-Forwarded-Proto  \$scheme;
+          grpc_set_header X-Forwarded-Port   \$server_port;
+          grpc_set_header Host               \$host;
+          grpc_set_header X-Forwarded-Host   \$host;
+          }
+ 	#Xray Config Path
+	location ~ ^/(?<fwdport>\d+)/(?<fwdpath>.*)\$ {
+		if (\$hack = 1) {return 404;}
+		client_max_body_size 0;
+		client_body_timeout 1d;
+		grpc_read_timeout 1d;
+		grpc_socket_keepalive on;
+		proxy_read_timeout 1d;
+		proxy_http_version 1.1;
+		proxy_buffering off;
+		proxy_request_buffering off;
+		proxy_socket_keepalive on;
+		proxy_set_header Upgrade \$http_upgrade;
+		proxy_set_header Connection "upgrade";
+		proxy_set_header Host \$host;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		#proxy_set_header CF-IPCountry \$http_cf_ipcountry;
+		#proxy_set_header CF-IP \$realip_remote_addr;
+		if (\$content_type ~* "GRPC") {
+			grpc_pass grpc://127.0.0.1:\$fwdport\$is_args\$args;
+			break;
+		}
+		if (\$http_upgrade ~* "(WEBSOCKET|WS)") {
+			proxy_pass http://127.0.0.1:\$fwdport\$is_args\$args;
+			break;
+	        }
+		if (\$request_method ~* ^(PUT|POST|GET)\$) {
+			proxy_pass http://127.0.0.1:\$fwdport\$is_args\$args;
+			break;
+		}
+	}
+	location / { try_files \$uri \$uri/ =404; }
+EOF
+
+/usr/local/x-ui/x-ui cert -webCert "/root/cert/${domain}/fullchain.pem" -webCertKey "/root/cert/${domain}/privkey.pem"
+x-ui restart
+systemctl restart nginx
+
