32810babd00394b3fd42f7c4cd97ee8166497e91
Ivan Razin <mozaroc@gmail.com>
2026-06-08 16:25:28 +0300

    Update x-ui-latest.sh

--- x-ui-latest.sh
+++ x-ui-latest.sh
@@ -544,7 +544,7 @@ if [[ -f $XUIDB ]]; then
              INSERT INTO "settings" ("key", "value") VALUES ("subPort",  '${sub_port}');
 	     INSERT INTO "settings" ("key", "value") VALUES ("subPath",  '/${sub_path}/');
 	     INSERT INTO "settings" ("key", "value") VALUES ("subURI",  '${sub_uri}');
-             INSERT INTO "settings" ("key", "value") VALUES ("subJsonPath",  '${json_path}');
+             INSERT INTO "settings" ("key", "value") VALUES ("subJsonPath",  '/${json_path}');
 	     INSERT INTO "settings" ("key", "value") VALUES ("subJsonURI",  '${json_uri}');
 		 INSERT INTO "settings" ("key", "value") VALUES ("subClashEnable",  'false');
 		 INSERT INTO "settings" ("key", "value") VALUES ("subEnableRouting",  'false');
@@ -1778,7 +1778,7 @@ apt-get update && apt-get install -y -q wget curl tar tzdata
             fi
         fi
         echo -e "Got x-ui latest version: ${tag_version}, beginning the installation..."
-        wget -N -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/v2.9.4/x-ui-linux-$(arch).tar.gz
+        wget -N -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access GitHub ${plain}"
             exit 1
@@ -1793,7 +1793,7 @@ apt-get update && apt-get install -y -q wget curl tar tzdata
             exit 1
         fi
         
-        url="https://github.com/MHSanaei/3x-ui/releases/download/v2.9.4/x-ui-linux-$(arch).tar.gz"
+        url="https://github.com/MHSanaei/3x-ui/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz"
         echo -e "Beginning to install x-ui $1"
         wget -N -O /usr/local/x-ui-linux-$(arch).tar.gz ${url}
         if [[ $? -ne 0 ]]; then
